d5e3ab492830f766f94fe8f8c368d0e6bf07f3e6
DenisGavar <tskhelp@gmail.com>
2024-10-13 20:44:16 +0300

    added table migrations

---
+++ migrations/20241013164010_create_products_table.js
@@ -0,0 +1,29 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("products", (table) => {
+    table.increments("id").primary();
+    table.string("name").notNullable();
+    table.text("description").notNullable();
+    table.decimal("price", 10, 2).notNullable();
+    table.integer("vendorId").unsigned().notNullable();
+
+    table
+      .foreign("vendorId")
+      .references("id")
+      .inTable("vendors")
+      .onDelete("CASCADE");
+
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("products");
+};
---
+++ migrations/20241013164015_create_orders_table.js
@@ -0,0 +1,27 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("orders", (table) => {
+    table.increments("id").primary();
+    table.integer("userId").unsigned().notNullable();
+    table.decimal("totalPrice", 10, 2).notNullable();
+
+    table
+      .foreign("userId")
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE");
+
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("orders");
+};
